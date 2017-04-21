ee3f6d8cff2dac126faa20590b6669ee8d4ed306
razaborg <razaborg@users.noreply.github.com>
2017-04-21 11:54:05 +0200

    first commit (HSS+MME available separately)

---
+++ HSS/startup.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# STEP 1 : mysql setup 
+mkdir -p /usr/local/etc/oai/freeDiameter
+sed -e "s/@MYSQL_user@/${MYSQL_user}/" -e "s/@MYSQL_pass@/${MYSQL_pass}/" ETC/hss.conf > /usr/local/etc/oai/hss.conf 
+
+# STEP 2 : config file creation
+cp ETC/acl.conf ETC/hss_fd.conf /usr/local/etc/oai/freeDiameter
+
+# STEP 3 : configure FQDN
+echo "Hostname: $(hostname)"
+
+# STEP 4 : check certificates
+./SCRIPTS/check_hss_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname)
+
+# Mysql daemon run
+service mysql start
+mysql -u root -e "create database oai_db"
+mysql -u root oai_db < ./SRC/OAI_HSS/db/oai_db.sql
+
+# HSS daemon start
+./SCRIPTS/run_hss -i ./SRC/OAI_HSS/db/
---
+++ MME/startup.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+cd /root/openair-cn
+mkdir -p /usr/local/etc/oai/freeDiameter
+
+
+
+cp /config/mme.conf /usr/local/etc/oai/
+
+cp /config/mme_fd.conf /usr/local/etc/oai/freeDiameter/
+
+./SCRIPTS/build_mme --clean
+
+./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
+
+#lsmod 
+
+./SCRIPTS/run_mme 
