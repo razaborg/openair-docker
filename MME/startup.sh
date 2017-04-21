4941a88b2e4f23a6cd024e68e75c44c9d5adca3b
razaborg <razaborg@users.noreply.github.com>
2017-04-21 12:10:42 +0200

    Cleaned the config files

--- MME/startup.sh
+++ MME/startup.sh
@@ -3,8 +3,6 @@
 cd /root/openair-cn
 mkdir -p /usr/local/etc/oai/freeDiameter
 
-
-
 cp /config/mme.conf /usr/local/etc/oai/
 
 cp /config/mme_fd.conf /usr/local/etc/oai/freeDiameter/
@@ -13,6 +11,5 @@ cp /config/mme_fd.conf /usr/local/etc/oai/freeDiameter/
 
 ./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
 
-#lsmod 
 
 ./SCRIPTS/run_mme 
