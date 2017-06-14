3973d862d4ba04714861f5c3a2e1db7343c91e93
razaborg <contact@razaborg.fr>
2017-06-14 17:13:42 +0200

    Completed the startup script to run the mme component.

--- MME/startup.sh
+++ MME/startup.sh
@@ -13,17 +13,16 @@ export S1C_IP=$(ip addr show ${S1C_IFACE} | grep 'inet ' | awk '{ print $2}')
 export S11_IFACE="eth1"
 export S11_IP=$(ip addr show ${S11_IFACE} | grep 'inet ' | awk '{ print $2}')
 
+envsubst < /config/mme.conf > /usr/local/etc/oai/mme.conf
+
+
 # Et pour avoir l'IP des autres composants --> résolution DNS \o/
-HSS_HOST="hss"
-HSS_IP=$(host ${HSS_HOST} | egrep -o "has address [0-9]+.+" | cut -d " " -f3)
+export HSS_HOST="hss"
+export HSS_IP=$(host ${HSS_HOST} | egrep -o "has address [0-9]+.+" | cut -d " " -f3)
 
-envsubst < /config/mme.conf > /usr/local/etc/oai/mme.conf && cat /usr/local/etc/oai/mme.conf
-
-envsubst < /config/mme_fd.conf > /usr/local/etc/oai/freeDiameter/mme_fd.conf && cat /usr/local/etc/oai/freeDiameter/mme_fd.conf
+envsubst < /config/mme_fd.conf > /usr/local/etc/oai/freeDiameter/mme_fd.conf 
 
 #./SCRIPTS/build_mme --clean
 
-#./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
-
-
-#./SCRIPTS/run_mme 
+./scripts/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
+./scripts/run_mme 
