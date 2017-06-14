8bb7bac56ed9ed2f0868813d8e70e21661caecb2
razaborg <contact@razaborg.fr>
2017-06-14 17:00:48 +0200

    Reconfigured MME network
    - All interfaces of MME are now represented in the docker-compose file and
    nommed from their EPC official name
    - Every container IP is now dynamically allowed by docker
    - Edited the startup script of the MME to automatically find the IP of
    its interfaces
    - Edited the config files with env variables to be able to manipulate it
    with envsubst in the startup script
    - The HSS address is obtained with the host command in the startup
    script
    - Modified the Dockerfile to embed dnsutils package

--- MME/startup.sh
+++ MME/startup.sh
@@ -3,13 +3,27 @@
 cd /root/openair-cn
 mkdir -p /usr/local/etc/oai/freeDiameter
 
-cp /config/mme.conf /usr/local/etc/oai/
+# Get the IP configuration
+export S6A_IFACE="eth0"
+export S6A_IP=$(ip addr show ${S6A_IFACE} | grep 'inet ' | awk '{ print $2}')
 
-cp /config/mme_fd.conf /usr/local/etc/oai/freeDiameter/
+export S1C_IFACE="eth0"
+export S1C_IP=$(ip addr show ${S1C_IFACE} | grep 'inet ' | awk '{ print $2}')
+
+export S11_IFACE="eth1"
+export S11_IP=$(ip addr show ${S11_IFACE} | grep 'inet ' | awk '{ print $2}')
+
+# Et pour avoir l'IP des autres composants --> résolution DNS \o/
+HSS_HOST="hss"
+HSS_IP=$(host ${HSS_HOST} | egrep -o "has address [0-9]+.+" | cut -d " " -f3)
+
+envsubst < /config/mme.conf > /usr/local/etc/oai/mme.conf && cat /usr/local/etc/oai/mme.conf
+
+envsubst < /config/mme_fd.conf > /usr/local/etc/oai/freeDiameter/mme_fd.conf && cat /usr/local/etc/oai/freeDiameter/mme_fd.conf
 
 #./SCRIPTS/build_mme --clean
 
-./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
+#./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
 
 
-./SCRIPTS/run_mme 
+#./SCRIPTS/run_mme 
