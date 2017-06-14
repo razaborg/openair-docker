5b0eef90a015b4fbf08b2652ad369288429b8792
razaborg <contact@razaborg.fr>
2017-06-14 07:22:24 +0200

    Moved the build MME from the startup script to the Dockerfile

--- MME/startup.sh
+++ MME/startup.sh
@@ -7,7 +7,7 @@ cp /config/mme.conf /usr/local/etc/oai/
 
 cp /config/mme_fd.conf /usr/local/etc/oai/freeDiameter/
 
-./SCRIPTS/build_mme --clean
+#./SCRIPTS/build_mme --clean
 
 ./SCRIPTS/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname -f)
 
